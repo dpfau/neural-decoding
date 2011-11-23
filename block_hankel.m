2ad15f1dddb8169b144a53cb452f835c86f8d38d
David Pfau <hotmail.com@gmail.com>
2011-11-23 00:00:19 -0500

    Significant housecleaning.  Moved, renamed or deleted most files.

--- block_hankel.m
+++
@@ -1,9 +0,0 @@
-function y = block_hankel( x, i, s, n )
-
-k = size(x,1);
-assert( size(x,2) >= n );
-y = zeros(s*k, n-s+1);
-
-for t = 0:(s-1)
-    y(t*k+(1:k),:) = x(:,(i+t):(i+t+n-s));
-end
--- test_trace.m
+++
@@ -1,7 +0,0 @@
-A = randn(n,m);
-B = randn(n,m);
-
-[u,s,v] = svd(A);
-[uu,ss,vv] = svd(B);
-
-trace(A*vv*ss'*uu') - trace(A*v*ss'*u')
