a89040cc1236ec6bca74f578460a87e785bb8f28
David Pfau <hotmail.com@gmail.com>
2012-01-26 12:36:47 -0500

    Added script to generate from predictive distribution for RR-HMM

---
+++ lds/rr-hmm/generate.m
@@ -0,0 +1,23 @@
+function x_t1 = generate( b_t, Q, c, b_inf, B_x )
+% Given a current belief state, generate an observation from the predictive
+% distribution
+%
+% Inputs:
+%   b_t - current belief state
+%   Q - square-root covariance of the kernels.  Includes kernel bandwidth
+%       implicitly
+%   c - kernel centers
+%   b_inf, B_x - parameters of the observable HMM representation 
+%
+% Output:
+%   x_t1 - generated data point
+
+assert( size(c,2) == size(B_x,3) );
+
+prob = zeros(size(c,2));
+for i = 1:size(c,2)
+    prob(i) = b_inf'*B_x(:,:,i)*b_t;
+end
+idx = find( cumsum(prob) > rand*sum(prob), 1 );
+
+x_t1 = c(:,idx) + Q*randn;
