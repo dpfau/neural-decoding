90e345ed0e5819bf5fd1ab41be948063a6a09514
David Pfau <hotmail.com@gmail.com>
2012-02-07 00:38:13 -0500

    Switched interpolation function due to singularity in second derivative, gradient debugged, still debugging second derivative of interpolation function.

--- lds/circular/e_step.m
+++ lds/circular/e_step.m
@@ -23,16 +23,33 @@ function [map prec] = e_step( data, params )
 %opts = struct( 'GradObj', 'on', 'Display', 'iter', 'LargeScale', 'on', 'Hessian', 'on', 'HessMult', @hess_mult );
 map = 0:params.dt:params.dt*(length(data)-1); % Initialize with no variance in phase shift
 %map = fminunc(@(x) log_lik( data, x, params ), map, opts );
-for t = 1:10
-    [~,grad,Hinfo] = log_lik( data, map, params );
-    v = randn(size(grad));
-    hv = hess_mult( Hinfo, v );
-    map = map + v*1e-8;
-    [~,grad2,~] = log_lik( data, map, params );
-    map = map - v*1e-8;
-    hv2 = (grad2-grad)/1e-8;
-    fprintf('Exact norm: %d, Approx norm: %d, Diff norm: %d\n',norm(hv),norm(hv2),norm(hv-hv2));
-end
+% for t = 1:10
+%     [~,grad,Hinfo] = log_lik( data, map, params );
+%     v = randn(size(grad));
+%     hv = hess_mult( Hinfo, v );
+%     map = map + v*1e-8;
+%     [~,grad2,~] = log_lik( data, map, params );
+%     map = map - v*1e-8;
+%     hv2 = (grad2-grad)/1e-8;
+%     for i = 1:length(hv)
+%         fprintf('Exact value: %d, Approx value: %d, Difference: %d\n',norm(hv(i)),norm(hv2(i)),norm(hv(i)-hv2(i)));
+%     end
+% end
+test_d2sinct(map,params);
+
+function test_d2sinct( map, params )
+
+T = length(params.template);
+coord = interp_coord( map, T );
+d1 = - pi*sin(pi*coord)./(T^2*sin(pi*coord/T).^2);
+
+map = map + 1e-8;
+coord2 = interp_coord( map, T );
+d2 = - pi*sin(pi*coord2)./(T^2*sin(pi*coord2/T).^2);
+
+dd1 = (d2-d1)/1e-8;
+dd2 = 2*pi*cos(pi*coord/T).*sin(pi*coord)./(T^3*sin(pi*coord/T).^3) - pi^2*cos(pi*coord)./(T^2*sin(pi*coord/T).^2);
+pause(1)
 
 function [ll grad Hinfo] = log_lik( data, x, params )
 
@@ -42,19 +59,22 @@ ll = sum( -( interp( coord, params.template ) - data ).^2/(2*params.sig_temp^2) 
     -( x(1) - params.t0 )^2/(2*params.sig_dt^2);
 
 grad = zeros(size(x));
-grad(1) = (params.t0 - x(1))/params.sig_dt^2;
-grad(1:end-1) = grad(1:end-1) - ( x(1:end-1) + params.dt - x(2:end) )/params.sig_dt^2;
-grad(2:end)   = grad(2:end)   - ( x(2:end) - params.dt - x(1:end-1) )/params.sig_dt^2;
+% grad(1) = (params.t0 - x(1))/params.sig_dt^2;
+% grad(1:end-1) = grad(1:end-1) - ( x(1:end-1) + params.dt - x(2:end) )/params.sig_dt^2;
+% grad(2:end)   = grad(2:end)   - ( x(2:end) - params.dt - x(1:end-1) )/params.sig_dt^2;
 grad = grad + ( data - interp( coord, params.template ) ).*dinterp( coord, params.template )/params.sig_temp^2;
 
-Hinfo = struct('diag',-2/params.sig_dt^2*ones(size(grad)),'off_diag',ones(1,size(grad,2)-1)/params.sig_dt^2);
-Hinfo.diag(end) = -1/params.sig_dt^2;
 Hinfo = struct('diag',zeros(size(grad)),'off_diag',zeros(1,size(grad,2)-1));
+% Hinfo = struct('diag',-2/params.sig_dt^2*ones(size(grad)),'off_diag',ones(1,size(grad,2)-1)/params.sig_dt^2);
+% Hinfo.diag(end) = -1/params.sig_dt^2;
+% Hinfo = struct('diag',zeros(size(grad)),'off_diag',zeros(1,size(grad,2)-1));
 Hinfo.diag = Hinfo.diag + ( -(dinterp( coord, params.template ).^2) + ...
     ( data - interp( coord, params.template ) ).*d2interp( coord, params.template ) )/params.sig_temp^2;
 
-ll = -ll; grad = -grad; % Hack because I want to maximize, not minimize.
-Hinfo.diag = -Hinfo.diag; Hinfo.off_diag = -Hinfo.off_diag;
+ll = -ll; 
+grad = -grad; % Hack because I want to maximize, not minimize.
+Hinfo.diag = -Hinfo.diag; 
+Hinfo.off_diag = -Hinfo.off_diag;
 
 function hv = hess_mult( Hinfo, v )
 % Multiplies the vector v by the Hessian of the log likelihood, with
@@ -63,21 +83,22 @@ function hv = hess_mult( Hinfo, v )
 hv = Hinfo.diag.*v + [0,Hinfo.off_diag.*v(1:end-1)] + [Hinfo.off_diag.*v(2:end),0];
 
 function y = interp( coord, template ) 
-% Whittaker-Shannon interpolation
+% Whittaker-Shannon interpolation, approximately.  Instead of using
+% sin(pi*x)/(pi*x), use sin(pi*x)/(T*tan(pi*x/T)).  Not the exact 
+% interpolation formula (terms go to zero too quickly far away from the 
+% center), but close enough for our purposes.
 
-y = template'*sinc(coord); 
+y = template'*sinct(coord,length(template)); 
 
 function y = dinterp( coord, template ) 
 % derivative of Whittaker-Shannon interpolation wrt time at which we are interpolating
 
-y = template'*dsinc(coord)*length(template)/2/pi;
-y(isnan(y)) = 0;
+y = template'*dsinct(coord,length(template))*length(template)/2/pi;
 
 function y = d2interp( coord, template )
 % second derivative of Whittaker-Shannon interpolation
 
-y = template'*d2sinc(coord)*length(template)^2/4/pi^2;
-y(isnan(y)) = 0;
+y = template'*d2sinct(coord,length(template))*length(template)^2/4/pi^2;
 
 function coord = interp_coord( x, N ) 
 % Given continuous time points and the period of a signal, returns matrix 
@@ -89,10 +110,17 @@ coord = ones(N,1)*phase - (1:N)'*ones(1,size(phase,2));
 coord( coord > N/2 )  = coord( coord > N/2 )  - N; % shift difference between phase and template points from (-2*pi,2*pi) to (-pi,pi)
 coord( coord < -N/2 ) = coord( coord < -N/2 ) + N;
 
-function y = dsinc( x ) % derivative of sin(pi*x)/(pi*x) wrt x
+function y = sinct( x, T )
 
-y = cos(pi*x)./x - sin(pi*x)./x.^2/pi;
+y = sin(pi*x)./(T*tan(pi*x/T));
+y(isnan(y)) = 1;
 
-function y = d2sinc( x ) % second derivative of sinc(x) = sin(pi*x)/(pi*x)
+function y = dsinct( x, T ) % derivative of sin(pi*x)/(T*tan(pi*x/T)) wrt x
 
-y = 2*sin(pi*x)./x.^3/pi - 2*cos(pi*x)./x.^2 - pi*sin(pi*x)./x;
+y = pi*cos(pi*x)./(T*tan(pi*x/T)) - pi*sin(pi*x)./(T^2*sin(pi*x/T).^2);
+y(isnan(y)) = 0; % deal with the singularity at 0
+
+function y = d2sinct( x, T ) % second derivative of sinct(x,T) = sin(pi*x)/(T*tan(pi*x/T))
+
+y = -pi^2*sin(pi*x)./(T*tan(pi*x/T)) - 2*pi^2*cos(pi*x)./(T^2*sin(pi*x/T).^2) + pi^2*cos(pi*x/T).*sin(pi*x)./(T^3*sin(pi*x/T).^3);
+y(isnan(y)) = -pi;
