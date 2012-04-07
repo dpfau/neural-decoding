7606536e64e335dd7b13b0afa9673255d6655295
David Pfau <hotmail.com@gmail.com>
2012-04-07 01:09:32 -0400

    Debugged custom interior point method for minimization with symmetry and semidefiniteness constraint.  Still have to add another constraint to ensure that output noise covariance is semidefinite.

--- lds/ssid/fit_noise.m
+++ lds/ssid/fit_noise.m
@@ -1,10 +1,40 @@
 function [Q R S] = fit_noise(e, A, C)
+% Estimate the noise covariances for the linear dynamical system:
+% x(t+1) = A*x(t) + B*u(t) + v(t)
+% y(t)   = C*x(t) + D*u(t) + w(t)
+% v(t) ~ N(0,Q)
+% w(t) ~ N(0,R)
+% By looking at the covariance between residuals at different time steps,
+% where the residuals are the difference between the observed y(t) and the
+% estimated y(t) assuming zero noise.
+%
+% Input:
+%   e - the difference between the observed y and the y reconstructed from
+%       a noiseless linear dynamical system
+%   C - the estimated latent-state-to-output matrix
+%   A - the estimated latent state evolution matrix
+%   s - the maximum time lag for which we use the covariance.  If dim(x) <
+%       dim(y) then asymptotically we'd never need s > 1, but in practice it
+%       helps
+%
+% Output:
+%   Q - Latent state noise, which introduces covariance between residuals
+%       at different time steps
+%   R - Output noise, which is uncorrelated from step to step
+%   S - The stationary covariance of the difference between the noisy and
+%       noiseless latent state, or Q + A*Q*A' + A^2*Q*A'^2 + ...
+%
+% Davi Pfau, 2012
 
 E0 = e*e'/size(e,2);
 E1 = e(:,2:end)*e(:,1:end-1)'/(size(e,2)-1);
 
-S = randn(size(A));
-S = S*S'; % Initialize at a feasible point
+Q0 = randn(size(A));
+Q0 = Q0*Q0'; 
+S = Q0;
+for i = 1:1000 % Initialize at a feasible point
+    S = Q0 + A*S*A';
+end
 symm = zeros(size(S,1)*(size(S,1)-1)/2,numel(S));
 t = 0;
 for i = 1:size(S,1)
@@ -16,8 +46,8 @@ for i = 1:size(S,1)
 end
 fprintf('Iter\tf(x)\tmax(imag(eig))\tmin(real(eig))\n');
 fprintf('%2.4d\t%2.4d\t%2.4d\t%2.4d\n',0,obj(S(:),E1,A,C,1),max(imag(eig(reshape(S,size(A))))),min(real(eig(reshape(S,size(A))))));
-for t = 1:100
-    [S,fval] = constrained_newton(@(x) obj(x,E1,A,C,1000), S(:), symm, zeros(size(symm,1),1), 1e-8);
+for t = 1:60
+    [S,fval] = constrained_newton(@(x) obj(x,E1,A,C,1e10*2^-t), S(:), symm, zeros(size(symm,1),1), 1e-8);
     fprintf('%2.4d\t%2.4d\t%2.4d\t%2.4d\n',t,fval,max(imag(eig(reshape(S,size(A))))),min(real(eig(reshape(S,size(A))))));
 end
 S = reshape(S,size(A));
@@ -29,7 +59,7 @@ function [f grad hess] = obj(X,E,A,C,t)
 X = reshape(X,size(A));
 foo = E-C*A*X*C';
 bar = inv(X-A*X*A')';
-f    = 0.5*norm(foo,'fro')^2 - t*log(det(bar));
+f    = 0.5*norm(foo,'fro')^2 - t*log(det(X-A*X*A'));
 grad = -A'*C'*foo*C - t*(bar-A'*bar*A);
 hess = zeros(numel(X));
 for i = 1:numel(X)
@@ -45,16 +75,21 @@ assert( size(b,2) == 1, 'b must be vector' );
 x = x0;
 fx_ = Inf;
 [fx,grad,hess] = f(x);
+t = 1;
+%fprintf('Iter \t f(x) \t\tmin eig \n')
 while abs(fx - fx_) > eps
+    %fprintf('%2.4d \t %2.4d \t %2.4d \n',t,fx,min(eig(reshape(x,7,7))));
+    t = t+1;
     foo = [hess, A'; A, zeros(numel(b))]\[-grad(:); b];
     dx = foo(1:length(x));
+    fx_ = fx;
     a = 1;
-    while f(x + a*dx) > fx + eps
+    [fx,grad,hess] = f(x + a*dx);
+    while imag(fx) ~= 0 || fx > fx_ % crossed the boundary
         a = a/2;
+        [fx,grad,hess] = f(x + a*dx);
     end
     x = x + a*dx;
-    fx_ = fx;
-    [fx,grad,hess] = f(x);
 end
     
 function Hv = hess_mult(X,A,C,t,v)
