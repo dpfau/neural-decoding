6f0aa683f9160f85d4a9eb55d3870a0a8cce65b0
David Pfau <hotmail.com@gmail.com>
2012-05-09 14:47:59 -0400

    Found the bug in the gradient calculations.  Turns out Q after several EM iterations Q was not perfectly symmetric, and that was amplified in Q^-1.

--- lds/plds/test_log_lik_grad.m
+++ lds/plds/test_log_lik_grad.m
@@ -1,9 +1,12 @@
-function test_log_lik_grad( data, map, params )
+function test_log_lik_grad( data, map, params, eps )
 
+if nargin < 4
+    eps = 1e-8;
+end
 [ll,grad,~] = log_lik( data, map, params );
 for i = 1:numel(map)
-    map(i) = map(i) + 1e-8;
+    map(i) = map(i) + eps;
     ll_ = log_lik( data, map, params );
-    map(i) = map(i) - 1e-8;
-    fprintf('Exact grad: %2.4d\t Approx grad: %2.4d\t Diff: %2.4d\n',grad(i),(ll_-ll)/1e-8,grad(i)-(ll_-ll)/1e-8);
+    map(i) = map(i) - eps;
+    fprintf('Exact grad: %2.4d\t Approx grad: %2.4d\t Diff: %2.4d\n',grad(i),(ll_-ll)/eps,grad(i)-(ll_-ll)/eps);
 end
