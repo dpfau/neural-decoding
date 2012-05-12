22495a7d1724b7c6f69ceefe49c6bed8c19505f9
David Pfau <hotmail.com@gmail.com>
2012-05-12 19:21:37 -0400

    Tracked down some bugs stemming from a failure to deal with NaNs arising during likelihood calculations, and added a second calling convention for data_log_lik for cases where we are evaluating held out data and haven't already calculated the Laplace approximation to the posterior of the latent path.

--- lds/plds/em.m
+++ lds/plds/em.m
@@ -5,8 +5,13 @@ function params = em( data, eps, k, maxIter, init )
 addpath ../../util
 addpath ../ssid % for block_hankel
 addpath /Users/davidpfau/Documents/MATLAB/tprod
-if nargin < 5
-    [params map] = rand_init( k, size(data,1), size(data,2) ); % initial estimate for phase path and parameters
+if nargin < 5 || strcmp(class(init),'double')
+    if nargin < 5
+        t = 0;
+    else
+        t = init;
+    end
+    [params map] = rand_init( k, size(data,1), size(data,2), t ); % initial estimate for phase path and parameters
 else
     map = init.map;
     params = init.params;
@@ -15,12 +20,13 @@ end
 dat_ll_ = Inf;
 dat_ll  = data_log_lik( data, map, prec, params );
 i = 0;
-fprintf('Iter \t Data LL \t Latent LL\n')
-while i < maxIter && ( i < 10 || dat_ll - dat_ll_ < eps )
+fprintf('Iter \t E Data LL \t M Data LL \t Latent LL\n')
+while i < maxIter %&& ( i < 10 || dat_ll - dat_ll_ < eps )
     i = i+1;
     [map,cll,prec] = newtons_method(@(x) log_lik( data, x, params ), map, 1e-8 ); % Initialize with path from previous step
+    dat_ll__ = data_log_lik( data, map, prec, params );
     params = m_step( data, map, prec, params );
     dat_ll_ = dat_ll;
     dat_ll  = data_log_lik( data, map, prec, params );
-    fprintf('%4i \t %2.4d \t %2.4d \n',i,-dat_ll,-cll);
+    fprintf('%4i \t %2.4d \t %2.4d \t %2.4d \n',i,-dat_ll__,-dat_ll,-cll);
 end
