bd2f71fba575bd0c3abdef43e72b5e95851c74a5
David Pfau <hotmail.com@gmail.com>
2012-05-24 00:34:28 -0400

    Rewrote SGPLVM in object-oriented paradigm, and implemented most of L-BFGS.  None of this is tested yet!

--- gplvm/ScaledGPLVM.m
+++ gplvm/ScaledGPLVM.m
@@ -30,44 +30,77 @@ classdef ScaledGPLVM
             sgplvm.z = foo(:,1:d)';
         end
         
-        function [K d e] = kernel(obj)
+        function [K d e] = kernel(obj,fa,fb,fc)
+            if nargin == 1
+                fa = obj.a;
+                fb = obj.b;
+                fc = obj.c;
+            end
             zz = obj.z'*obj.z;
             d = diag(zz)*ones(1,obj.N) - 2*zz + ones(obj.N,1)*diag(zz)';
-            e = exp( -pbj.c/2*d );
-            K = obj.a*e + eye(obj.N)/obj.b;
+            e = exp( -fc/2*d );
+            K = fa*e + eye(obj.N)/fb;
         end
         
-        function [fy grad] = objective(obj)
+        function [fy grad] = f(obj,params)
             % Computes the objective as well as gradient wrt z,a,b,c,w
             % of the objective for the scaled GPLVM
             
-            [K d e] = kernel(obj);
-            wy = diag(obj.w)*obj.y;
+            if nargin == 2
+                fa = params(1);
+                fb = params(2);
+                fc = params(3);
+                fw = params(4:obj.D+3);
+                fz = reshape(params(obj.D+4:end),obj.d,obj.N);
+            else
+                fa = obj.a;
+                fb = obj.b;
+                fc = obj.c;
+                fw = obj.w;
+                fz = obj.z;
+            end
+            [K g e] = kernel(obj,fa,fb,fc);
+            wy = diag(fw)*obj.y;
             Ki = K^-1; % This is the biggest impediment to scaling
             fy = obj.D*sum(log(diag(chol(K)))) ...
                 + 1/2*sum(diag(wy*Ki*wy')) ...
-                + 1/2*obj.z(:)'*obj.z(:) ...
-                + log(obj.a) + log(obj.b) + log(obj.c) ...
-                - obj.N*sum(log(obj.w));
+                + 1/2*fz(:)'*fz(:) ...
+                + log(fa) + log(fb) + log(fc) ...
+                - obj.N*sum(log(fw));
             dK = (-(Ki*wy')*(wy*Ki) + pbj.D*Ki)/2;
-            dz = -obj.c*(tprod(K,[2 3],obj.z,[1 2])-tprod(K,[2 3],obj.z,[1 3]));
-            grad = struct( 'z', 2*tprod(dK,[2 -1],dz,[1 2 -1]) + pbj.z, ...
-                'a', trace(dK*e) + 1/obj.a, ...
-                'b', trace(-dK/obj.b^2) + 1/obj.b, ...
-                'c', trace(-1/2*dK*(d.*K)) + 1/obj.c, ...
-                'w', obj.w.*diag(obj.y*Ki*obj.y')'-obj.N./obj.w );
+            dz1 = -fc*(tprod(K,[2 3],fz,[1 2])-tprod(K,[2 3],fz,[1 3]));
+            dz = 2*tprod(dK,[2 -1],dz1,[1 2 -1]) + fz;
+            dw = fw.*diag(obj.y*Ki*obj.y')'-obj.N./fw;
+            grad = [ trace(dK*e) + 1/fa; ...
+                     trace(-dK/fb^2) + 1/fb; ...
+                     trace(-1/2*dK*(g.*K)) + 1/fc; ...
+                     dw(:); ...
+                     dz(:) ];
         end
         
-        function test_grad()
-            [f,grad] = sgplvm_obj(y,params);
-            for var = fieldnames(params)'
-                fprintf('%s:\n', char(var))
-                for i = 1:numel(params.(char(var)))
-                    params.(char(var))(i) = params.(char(var))(i) + 1e-5;
-                    f_ = sgplvm_obj(y,params);
-                    params.(char(var))(i) = params.(char(var))(i) - 1e-5;
-                    fprintf('Numeric: %d, Analytic: %d\n',(f_-f)/1e-5,grad.(char(var))(i));
-                end
+        function obj = assign_params(obj,params)
+            % Because gradients are returned in long vector form, we need
+            % to be able to assign parameters from one long vector
+            
+            obj.a = params(1);
+            obj.b = params(2);
+            obj.c = params(3);
+            obj.w = params(4:obj.D+3);
+            obj.z = reshape(params(obj.D+4:end),obj.d,obj.N);
+        end
+        
+        function params = get_params(obj) 
+            params = [obj.a; obj.b; obj.c; obj.w(:); obj.z(:)];
+        end
+        
+        function test_grad(obj)
+            [f,grad] = obj.f();
+            params = obj.get_params;
+            for i = 1:length(params)
+                params(i) = params(i) + 1e-5;
+                f_ = obj.f(params);
+                params(i) = params(i) - 1e-5;
+                fprintf('Numeric: %d, Analytic: %d\n',(f_-f)/1e-5,grad(i));
             end
         end
     end
---
+++ gplvm/lbfgs.m
@@ -0,0 +1,75 @@
+function [y fy] = lbfgs(obj,m,tol,iter,display)
+
+if nargin < 5
+    display = 0;
+end
+if nargin < 4
+    iter = Inf;
+end
+c1 = 1e-4;
+c2 = 0.9; % Following the totally arbitrary numbers in Nocedal and Wright
+
+y = obj.get_params();
+[fy grad] = obj.f();
+k = 0;
+s = [];
+z = [];
+if display, fprintf('Iter \t f(y) \t ||grad||'), end
+while norm(grad) > tol && k < iter
+    if display, fprintf('%i \t %2.4d \t %2.4d \n',k,fy,norm(grad)), end
+    % Two loop recursion
+    a = zeros(1,min(m,k));
+    q = grad;
+    for i = min(m,k):-1:1
+        a(i) = s(:,i)'*q/(z(:,i)'*s(:,i));
+        q = q - a(i)*z(:,i);
+    end
+    if k == 0
+        r = q;
+    else
+        r = (s(:,end)'*z(:,end))/(z(:,end)'*z(:,end))*q;
+    end
+    for i = 1:min(m,k)
+        b = z(:,i)'*r/(z(:,i)'*s(:,i));
+        r = r + s(:,i)*(a(i)-b);
+    end
+    
+    % Backtracking line search
+    a0 = 0;
+    a1 = 1;
+    a_ = 100;
+    p = -r;
+    fy0 = fy;
+    i = 1;
+    while 1
+        [fy1 grad1] = obj.f(y+a1*p);
+        if fy1 > fy + c1*a1*grad'*p || ( fy1 >= fy0 && i > 1 )
+            a = zoom(a0,a1); % Need to define this!
+            break
+        end
+        if abs(grad1'*p) <= -c2*grad'*p
+            a = a1;
+            break
+        end
+        if grad1'*p >= 0
+            a = zoom(a0,a1);
+            break
+        end
+        a0 = a1;
+        a1 = mean(a0,a_);
+        i = i+1;
+        fy0 = fy1;
+    end
+    
+    % Update parameters
+    s = [s, a*p];
+    z = [z, grad_-grad];
+    obj.set_params(y+a*p);
+    k = k+1;
+    if k > m
+        s = s(:,2:end);
+        z = z(:,2:end);
+    end
+    fy = fy_;
+    grad = grad_;
+end
