ef6e4614a0e9fffd3270eda004ff6c4bc3620e35
David Pfau <hotmail.com@gmail.com>
2012-05-30 22:11:57 -0400

    Implemented conjugate of poisson log likelihood without bias or history terms.

--- lds/ssid/poiss_loglik_conj.m
+++ lds/ssid/poiss_loglik_conj.m
@@ -3,4 +3,8 @@ function [f df] = poiss_loglik_conj( y, k, s, x, t )
 if nargin == 3
     error( 'Function does not support proximity operator' )
 else
+    f = sum( sum( ( log( 1/k * x + y ) - 1 ) .* ( x + k * y ) ) );
+    if nargout == 2
+        df = log( 1/k * x + y );
+    end
 end
