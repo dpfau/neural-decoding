d6c5a00c90c024338b62074641eba0d12dc5a92d
David Pfau <hotmail.com@gmail.com>
2012-06-12 23:00:23 -0400

    Debugging Newton's method for finding conjugate of poisson log likelihood with bias.

--- lds/ssid/likelihoods/newton_root.m
+++ lds/ssid/likelihoods/newton_root.m
@@ -14,28 +14,31 @@ end
 z = z0;
 [fz Jz] = f( z );
 while norm( fz - x ) > eps
-    % Solve Jz*dz = -fz by stabilized biconjugate gradient
-    dz = zeros( numel( z ), 1 );
-    r  = -fz(:) - Jz*dz;
-    r_ = r;
-    p  = r;
-    p_ = r_;
-    while norm( Jz*dz + fz ) > eps
-        a = ( r_'*r )/( p_'*Jz*p );
-        
-        r1  = r  - a*Jz*p;
-        r_1 = r_ - a*Jz'*p_;
-        
-        b = ( r1'*r_1 )/( r'*r_ );
-        
-        p  = r1  + b*p;
-        p_ = r_1 + b*p_;
-        
-        r  = r1;
-        r_ = r_1;
-
-        dz = dz + a*p;
-    end
+    fprintf( '%d\n', norm( fz - x ) );
+%     % Solve Jz*dz = -fz by stabilized biconjugate gradient
+%     dz = zeros( numel( z ), 1 );
+%     r  = -fz(:) - Jz*dz;
+%     r_ = r;
+%     p  = r;
+%     p_ = r_;
+%     while norm( Jz*dz + fz(:) ) > eps
+%         fprintf( '\t%d\n', norm( Jz*dz + fz(:) ) );
+%         a = ( r_'*r )/( p_'*Jz*p );
+%         
+%         r1  = r  - a*Jz*p;
+%         r_1 = r_ - a*Jz'*p_;
+%         
+%         b = ( r1'*r_1 )/( r'*r_ );
+%         
+%         p  = r1  + b*p;
+%         p_ = r_1 + b*p_;
+%         
+%         r  = r1;
+%         r_ = r_1;
+% 
+%         dz = dz + a*p;
+%     end
+    dz = -Jz\fz(:);
     z = z + reshape( dz, size( z ) );
     [fz Jz] = f( z );
 end
--- lds/ssid/likelihoods/poiss_loglik_bias_conj.m
+++ lds/ssid/likelihoods/poiss_loglik_bias_conj.m
@@ -3,7 +3,24 @@ function [f df] = poiss_loglik_bias_conj( y, k, e, x, t )
 if nargin == 5
     error( 'Function does not support proximity operator' )
 else
-    z = newton_root( @(
+    z = newton_root( @(z) poiss_loglik_bias_jacobian( y, k, e, z ), x );
     [fz gradz] = poiss_loglik_bias( y, k, e, z );
-    f = z(:)'*gradz - fz;
+    f = z(:)'*gradz(:) - fz;
 end
+df = 0;
+
+function [fz Jz] = poiss_loglik_bias_jacobian( y, k, e, x )
+% Returns the gradient and the Jacobian of the gradient (that is, the
+% Hessian) of the log likeilihood of the poisson GLM with bias term, for
+% use in computing the x for which the gradient of poiss_loglik_bias = z,
+% needed to compute the convex conjugate of poiss_loglik_bias
+
+m = size( x, 1 );
+n = size( x, 2 );
+z = x( :, 1 : end - 1 ) + x( :, end ) * ones( 1, n - 1 );
+ez = exp( z );
+fz = k * [ -y + ez, sum( -y + ez, 2 ) + e * x( :, end ) ];
+i = [ 1 : m * ( n - 1 ), 1 : m * ( n - 1 ), repmat( m * ( n - 1 ) + ( 1 : m ), 1, n ) ] ;
+j = [ 1 : m * ( n - 1 ), repmat( m * ( n - 1 ) + ( 1 : m ), 1, n - 1 ), 1 : m * ( n - 1 ), m * ( n - 1 ) + ( 1 : m ) ];
+h = k*[ ez, ez, ez, e + sum( ez, 2 ) ];
+Jz = sparse( i, j, h, numel( x ), numel( x ) );
