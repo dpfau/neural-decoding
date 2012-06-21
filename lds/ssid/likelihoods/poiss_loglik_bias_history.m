0016f7b5fea06354bce889348e18fdbf8ff75dd9
David Pfau <hotmail.com@gmail.com>
2012-06-21 18:44:41 -0400

    Goddamn indices don't line up right.  I quit for today.

--- lds/ssid/likelihoods/poiss_loglik_bias_history.m
+++ lds/ssid/likelihoods/poiss_loglik_bias_history.m
@@ -17,13 +17,14 @@ else
             Y = block_hankel( [ my( :, ones( 1, s ) ), y ], 1, s, n + s - 1 );
             DY = D*Y;
         else
+            D  = 0;
             DY = 0;
         end
         z = x( :, 1 : n ) + x( :, n + 1 ) * ones( 1, n ) + DY;
         ez = exp( z );
         f = k * sum( sum( -y.*z + ez ) ) ...
             + 1/2 * k * e * x( :, n + 1 )' * x( :, n + 1 ) ...
-            + 1/2 * k * g * x( :, n + 2 : end )' * x( :, n + 2 : end );
+            + 1/2 * k * g * D( : )' * D( : );
         if nargout >= 2
             if s > 0
                 df = k * [ -y + ez, sum( -y + ez, 2 ) + e * x( :, n + 1 ), ( -y + ez ) * Y' + g * x( :, n + 2 : end ) ];
@@ -40,15 +41,17 @@ else
                     bd = m * n + ( 1 : m * m * s );
                     dd_i = repmat( bd, 1, m * s );
                     dd_j = repmat( reshape( bd, m, m * s ), m * s, 1 );
-                    i = [ xx, xx, xb, bb, repmat( xx, 1, m * s ), xd, repmat( bb, 1, m * s ), bd, dd_i ];
-                    j = [ xx, xb, xx, bb, xd, repmat( xx, 1, m * s ), bd, repmat( bb, 1, m * s ), dd_j ];
-                    h = k * [ ez, ez, ez, e + sum( ez, 2 ), ezY, ezY, sum( ezY, 3 ), sum( ezY, 3 ), g + tprod( tprod( ez, [1 3], Y, [2 3] ), [1 2 -1], Y, [3 -1] ) ];
+                    i = [ xx, xx, xb, bb, repmat( xx, 1, m * s ), xd( : )', repmat( bb, 1, m * s ), bd, dd_i ];
+                    j = [ xx, xb, xx, bb, xd( : )', repmat( xx, 1, m * s ), bd, repmat( bb, 1, m * s ), dd_j( : )' ];
+                    h = k * [ ez, ez, ez, e + sum( ez, 2 ), ...
+                         reshape( ezY, m, n * m * s ), reshape( ezY, m, n * m * s ), squeeze( sum( ezY, 2 ) ), squeeze( sum( ezY, 2 ) ), ...
+                         reshape( tprod( tprod( ez, [1 3], Y, [2 3] ), [1 2 -1], Y, [3 -1] ), m, m * m * s * s ) ];
                 else
-                    i = [ 1 : m * ( n - 1 ), 1 : m * ( n - 1 ), repmat( m * ( n - 1 ) + ( 1 : m ), 1, n ) ] ;
-                    j = [ 1 : m * ( n - 1 ), repmat( m * ( n - 1 ) + ( 1 : m ), 1, n - 1 ), 1 : m * ( n - 1 ), m * ( n - 1 ) + ( 1 : m ) ];
+                    i = [ 1 : m * n, 1 : m * n, repmat( m * n + ( 1 : m ), 1, n ) ] ;
+                    j = [ 1 : m * n, repmat( m * n + ( 1 : m ), 1, n - 1 ), 1 : m * n, m * n + ( 1 : m ) ];
                     h = k * [ ez, ez, ez, e + sum( ez, 2 ) ];
                 end
-                Hf = sparse( i, j, h, numel( x ), numel( x ) );
+                Hf = sparse( i, j, h, numel( x ), numel( x ) ) + g * sparse( m * n + ( 1 : m * m * s ), m * n + ( 1 : m * m * s ), ones( 1, m * m * s ) );
             end
         end
     end
