ff78558da45b614cb37d3c27c58a8835cb1ade31
David Pfau <hotmail.com@gmail.com>
2012-06-25 19:16:05 -0400

    Integrated convex conjugate code into MOESP, and rewrote convex conjugate code to use closures.

--- lds/ssid/test_poiss_nn.m
+++ lds/ssid/test_poiss_nn.m
@@ -6,6 +6,6 @@ opts.tol = '1e-3';
 opts.maxIter = 100;
 opts.instant = 0;
 opts.vsig = 25;
-opts.tfocs_path = '/Users/davidpfau/Documents/MATLAB/TFOCS1.1';
-opts.lag = 2;
+opts.tfocs_path = '/Users/davidpfau/Documents/MATLAB/TFOCS';
+opts.lag = 0;
 moesp(x,u,5,1000,opts);
--- util/make_convex_conjugate.m
+++ util/make_convex_conjugate.m
@@ -1,19 +1,30 @@
-function f_conj = make_convex_conjugate( f )
+function f_conj = make_convex_conjugate( f, z0 )
 % Returns function handle that numerically computes convex conjugate via Newton's method:
 % f*(x) = sup_z <x,z> - f(z)
 
+    if nargin == 1
+        z0 = f( [] );
+    end
+    
+    function [y grad hess] = conjugate_objective( f, x, z )
+        
+        [y grad hess] = f( z );
+        y = - x(:)'*z(:) + y;
+        grad = - x + grad;
+        
+    end
+
+    function [y grad] = convex_conjugate( f, x, t )
+        % TFOCS compatible calling convention
+        
+        if nargin == 3
+            error( 'Function does not support proximity operator' )
+        end
+        [grad,y] = newtons_method( @(z) conjugate_objective( f, x, z ), z0, 1e-6, 0 );
+        y = -y;
+        z0 = grad; % Closure!
+    end
+
 f_conj = @(varargin) convex_conjugate( f, varargin{:} );
 
-function [y grad hess] = conjugate_objective( f, x, z )
-
-[y grad hess] = f( z );
-y = - x(:)'*z(:) + y;
-grad = - x + grad;
-
-function y = convex_conjugate( f, x, z0 )
-
-if nargin == 2
-    z0 = f( [] );
 end
-[~,y] = newtons_method( @(z) conjugate_objective( f, x, z ), z0, 1e-6, 1 );
-y = -y;
