e3477cabf6a01b113fadac48f882034196863da5
David Pfau <hotmail.com@gmail.com>
2012-07-12 22:37:45 -0400

    Tightened convergence criterion in newton's method.

--- util/newtons_method.m
+++ util/newtons_method.m
@@ -3,15 +3,15 @@ function [x fx Hinfo] = newtons_method( f, x0, eps, verbose )
 if nargin < 4
     verbose = 0;
     if nargin == 2
-        eps = 1e-6;
+        eps = 1e-16;
     end
 end
 
-fx0 = Inf;
 [fx,grad,Hinfo]  = f(x0);
 
 i = 0;
-while fx0 - fx > eps
+decr = Inf;
+while decr > eps
     i = i+1;
     fx0 = fx;
     
@@ -21,7 +21,9 @@ while fx0 - fx > eps
         H = sparse_hess( Hinfo );
     end
     
-    x = reshape( x0(:) - H\grad(:), size(x0) );
+    dx = H\grad(:);
+    decr = grad(:)'*dx;
+    x = reshape( x0(:) - dx, size(x0) );
     while f(x) - eps >= fx0 || isnan(f(x)) % If we overshoot the minimum
         x = x0 + (x - x0)/2;
     end
